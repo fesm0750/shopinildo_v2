4ef5751c8e1bad1ecf7b894097da907fcea175dc
Fernando Martins <emmanuel.martins@outlook.com>
2024-03-22 14:02:28 -0300

    refact[CARRINHO]: remove description from response

--- carrinho-service/src/main/java/br/edu/postech/shopinildo/carrinho/dto/ItemDTO.java
+++ carrinho-service/src/main/java/br/edu/postech/shopinildo/carrinho/dto/ItemDTO.java
@@ -8,12 +8,13 @@ import br.edu.postech.shopinildo.carrinho.integration.item.ItemResponse;
 public record ItemDTO(
         String itemId,
         String name,
-        String description,
         int quantity,
         boolean isAvailable,
         BigDecimal price) {
 
     public ItemDTO(String itemId, int quantity, ItemAvailableDTO estoqueResponse, ItemResponse itemResponse) {
-        this(itemId, itemResponse.name(), itemResponse.description(), quantity, estoqueResponse.isAvailable(), itemResponse.price());
+        this(itemId, itemResponse.name(), quantity, estoqueResponse.isAvailable(), itemResponse.price());
     }
 }
+
+
